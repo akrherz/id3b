e899db0d86eac256755273cae9a6e2cf016550d2
akrherz <akrherz@iastate.edu>
2017-10-09 15:26:04 -0500

    WIP closer on database schema being finalized

--- database/init/id3b.sql
+++ database/init/id3b.sql
@@ -7,7 +7,12 @@ CREATE TABLE ldm_product_log(
   ldm_feedtype int,
   seqnum int,
   product_id varchar,
-  product_origin varchar
+  product_origin varchar,
+  wmo_ttaaii char(6),
+  wmo_source char(4),
+  wmo_valid_at timestamptz,
+  wmo_bbb char(3),
+  awips_id varchar(6)
 );
 GRANT ALL on ldm_product_log to ldm;
 GRANT SELECT on ldm_product_log to nobody,apache;
--- ldm/id3b_ingest.py
+++ ldm/id3b_ingest.py
@@ -4,17 +4,20 @@ from syslog import LOG_LOCAL2
 import StringIO
 import json
 import os
+import re
+import datetime
 
 from twisted.python import log, syslog
 from twisted.internet import stdio
 from twisted.internet import reactor
 from twisted.protocols import basic
-from twisted.internet import task
 from twisted.enterprise import adbapi
 
 
 from applib.parser import parser
 
+WMO_RE = re.compile((r"^([0-9A-Z]{4,6}) ([A-Z0-9]{4}) ([0-9]{6})( [A-Z]{3})?"
+                     r"( /p[A-Z0-9]{3,6})?"))
 syslog.startLogging(prefix='id3b_ingest', facility=LOG_LOCAL2)
 CFGFN = "%s/settings.json" % (os.path.join(os.path.dirname(__file__),
                                            "../config"),)
@@ -27,6 +30,17 @@ DBPOOL = adbapi.ConnectionPool('psycopg2', database=DBOPTS['name'],
                                password=DBOPTS['password'])
 
 
+def compute_wmo_time(valid, ddhhmm):
+    """Attempt to resolve the time!"""
+    day = int(ddhhmm[:2])
+    if day < 5 and valid.day > 24:
+        # Next month
+        valid += datetime.timedelta(days=15)
+    return valid.replace(day=day, hour=int(ddhhmm[2:4]),
+                         minute=int(ddhhmm[4:6]), second=0,
+                         microsecond=0)
+
+
 def handle_error(err):
     """Handle an error?"""
     print(err)
@@ -36,13 +50,30 @@ def save_msgs(txn, msgs):
     """Persist our message"""
     for msg in msgs:
         # print("%6s %s" % (msg.size, msg.product_id))
+        tokens = WMO_RE.findall(msg.product_id)
+        awips_id = None
+        wmo_ttaaii = None
+        wmo_source = None
+        wmo_time = None
+        wmo_bbb = None
+        awips_id = None
+        if tokens:
+            (wmo_ttaaii, wmo_source, wmo_time, wmo_bbb, awips_id) = tokens[0]
+            wmo_bbb = wmo_bbb.strip()
+            awips_id = awips_id[3:]
+            try:
+                wmo_time = compute_wmo_time(msg.valid, wmo_time)
+            except Exception as exp:
+                print("%s valid: %s wmo_time: %s" % (exp, msg.valid, wmo_time))
         txn.execute("""
         INSERT into ldm_product_log
         (md5sum, size, valid_at, ldm_feedtype,
-         seqnum, product_id, product_origin)
-        VALUES (%s, %s, %s, %s, %s, %s, %s)
+         seqnum, product_id, product_origin,
+         wmo_ttaaii, wmo_source, wmo_valid_at, wmo_bbb, awips_id)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """, (msg.md5sum, msg.size, msg.valid, msg.feedtype,
-              msg.seqnum, msg.product_id, msg.product_origin))
+              msg.seqnum, msg.product_id, msg.product_origin,
+              wmo_ttaaii, wmo_source, wmo_time, wmo_bbb, awips_id))
 
 
 class IngestorProtocol(basic.LineReceiver):
@@ -84,7 +115,7 @@ def main():
     # Something to store data between runs
     proto.leftover = b""
     _ = LDMProductFactory(proto)
-    reactor.run()
+    reactor.run()  # @UndefinedVariable
 
 
 if __name__ == '__main__':
